60e7fe147eaac10a71b516891151080270039ae8
destiny891116 <destiny891116@gmail.com>
2024-08-18 01:29:37 +0900

    css 수정

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -5,7 +5,8 @@ const WeatherBox = ({weather, id}) => {
   return (
     <div className="weather-box">
       <div>{weather?.name}</div> 
-      <h2>{weather && weather.main ? Math.round(weather.main.temp) : ""}℃  /  {weather && weather.main ? ((weather?.main.temp * 9) / 5 + 32).toFixed(2) : ""}℉</h2>
+      <h1>{weather && weather.main ? Math.round(weather.main.temp) : ""}℃</h1>
+      <h3>{weather && weather.main ? ((weather?.main.temp * 9) / 5 + 32).toFixed(2) : ""}℉</h3>
       <h3>{weather && weather.weather[0]?.description}</h3>
       <img className="img-fluid" src={`http://openweathermap.org/img/wn/${id}@2x.png`} />
     </div>
--- src/component/WeatherBtn.js
+++ src/component/WeatherBtn.js
@@ -4,10 +4,10 @@ import { Button } from 'react-bootstrap';
 const WeatherBtn = ({cities, setCity, handleCityChange}) => {
   return (
     <div>
-       <Button variant={`${setCity === null ? "outline-warning" : "warning"}`} onClick={() => handleCityChange("current")}>Current Location</Button>
+       <Button className="weatherBtn" variant={`${setCity === null ? "outline-warning" : "warning"}`} onClick={() => handleCityChange("current")}>Current Location</Button>
 
         {cities.map((item, index)=> (
-          <Button variant={`${setCity === item ? "outline-warning" : "warning"}`} onClick={() => handleCityChange(item)} >{item}</Button>
+          <Button className="weatherBtn" variant={`${setCity === item ? "outline-warning" : "warning"}`} onClick={() => handleCityChange(item)} >{item}</Button>
         ))};
     </div>
   )
