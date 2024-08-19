2223bda0d7ff45d8089538e4b8542a2110acc2cb
destiny891116 <destiny891116@gmail.com>
2024-08-19 20:11:32 +0900

    이미지액박 처리

--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -8,7 +8,7 @@ const WeatherBox = ({weather, id}) => {
       <h1>{weather && weather.main ? Math.round(weather.main.temp) : ""}°</h1>
       <h4>{weather && weather.main ? ((weather?.main.temp * 9) / 5 + 32).toFixed(1) : ""}℉</h4>
       <h4>{weather && weather.weather[0]?.description}</h4>
-      <img className="img-fluid" src={`http://openweathermap.org/img/wn/${id}@2x.png`} />
+      <img className="img-fluid" src={`http://openweathermap.org/img/wn/${id}@2x.png`} alt="data-img" onError={(e) => (e.target.style.display = "none")}/>
     </div>
   )
 }
